fc61eadbb94ac3b70b0b7db9efd28522ad2854f7
Assela Pathirana <assela@pathirana.net>
2022-11-05 18:35:10 +0100

    ts -> js

--- src/stores/data.js
+++ src/stores/data.js
@@ -6,24 +6,27 @@ export const useDataStore = defineStore('counter', () => {
   //computed()s become getters
   //function()s become actions
   // see https://pinia.vuejs.org/core-concepts/#setup-stores
+  //following are state properties:
   const data = ref([])
   const attr = ref({})
   const layout = ref({})
+  // set their initial values
   data.value=setData()
   attr.value= { displayModeBar: true}
   layout.value= { title: "My graph :)", margin: {l:20, r:20, t:30, b:20} }
 
+  //action to change data
   function changeData(){
     data.value=setData()
   }
-
-  const chartData = computed(() => ({data: JSON.parse(JSON.stringify(data.value)), 
-                                    attr: JSON.parse(JSON.stringify(attr.value)), 
-                                    layout: JSON.parse(JSON.stringify(layout.value)) }))
-
+  // getter for chartData
+  const chartData = computed(() => 
+  //Plotly seem to mutate these values. So, to ensure it does not happen, we deepcopy things. 
+            ({data: JSON.parse(JSON.stringify(data.value)), 
+              attr: JSON.parse(JSON.stringify(attr.value)), 
+              layout: JSON.parse(JSON.stringify(layout.value)) }))
+  // pinia requires to return the data
   return { data, attr, layout , changeData, chartData}
-  
-
 
 })
 

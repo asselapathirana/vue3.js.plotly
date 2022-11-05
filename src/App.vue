7d30cbaa3f2910b64c15e8de6ce62470bfd3d2fd
Assela Pathirana <assela@pathirana.net>
2022-11-05 10:47:50 +0100

    working with script setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PlotlyBarEx from './components/PlotlyBarEx.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <PlotlyBarEx  />
+    </div>
+  </header>
+</template>
+
---
+++ src/components/PlotlyBarEx.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><h1> Hello </h1>
+  <div style="width:500px;height:400px;" id="plot1"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import  Plotly  from 'plotly.js-dist/plotly'
+const data=[{ x: [1,2,3,4], y: [10,15,13,17], type:"scatter" }]
+const attr= { displayModeBar: true}
+const layout= { title: "My graph :)", margin: {l:20, r:20, t:30, b:20} }
+onMounted(() => {
+  Plotly.newPlot('plot1', data, layout)
+})
+
+</script>
